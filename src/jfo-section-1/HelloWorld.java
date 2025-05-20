a51e15d5ad9fa1a6c1b15cd115b0e8652787ca0c
hoshikuzuryuu <yaminoiroha@gmail.com>
2025-05-20 10:17:12 -0700

    dagasdasdadweq

--- src/jfo-section-1/HelloWorld.java
+++ src/jfo-section-1/HelloWorld.java
@@ -1,6 +1,16 @@
 public class HelloWorld {
 
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        byte b = 2;
+        long l = 6353413233214311325L;
+        short s = 1000;
+        
+        int i=0;
+        while(i<10)
+        {
+            System.out.println(i++);
+        }
     }
 }
--- src/jfo-section-3/Chickens01.java
+++ src/jfo-section-3/Chickens01.java
@@ -1,7 +1,26 @@
 public class Chickens01 {
     public static void main(String[] args) {
-        //Put yout code here
-        
-        System.out.println(totalEggs);
+        egg(3, 5);
+        egg(4,8);
     }   
+    public static void egg(int chickenCount, int eggsPerChicken)
+    {
+        int totalEggs = 0; 
+        for (int i = 0; i < 3; i++)
+        {
+            if (i == 1)
+            {
+                chickenCount += 1;
+            }
+            
+            if (i == 2)
+            {
+                chickenCount /= 2;
+            }
+
+            totalEggs += chickenCount*eggsPerChicken;
+        }
+        System.out.println(totalEggs);
+    }
 }
+
