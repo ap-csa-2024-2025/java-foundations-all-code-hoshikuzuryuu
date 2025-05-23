3e9af3efd21681c9ea900970ccc60088cd9357b2
hoshikuzuryuu <yaminoiroha@gmail.com>
2025-05-23 10:13:33 -0700

    adadsaddada

--- src/jfo-section-4/NameMaker.java
+++ src/jfo-section-4/NameMaker.java
@@ -1,9 +1,15 @@
+import javax.swing.JOptionPane;
+//import java.util.Scanner;
 
 public class NameMaker {
     
     public static void main(String args[])
     {
-        
+        String name = JOptionPane.showInputDialog("Enter your name");
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
+        String address= JOptionPane.showInputDialog("Enter yoru address");
+        String message = String.format("Your anme is %s%nYou are %d years old%nYou live at %s" , name, age, address);
+        System.out.println(message);
     }
     
 }
