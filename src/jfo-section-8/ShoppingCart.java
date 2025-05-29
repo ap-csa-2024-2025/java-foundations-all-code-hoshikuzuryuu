45dcf5b58f2686e3eebda0ede349f64684c8e68a
hoshikuzuryuu <yaminoiroha@gmail.com>
2025-05-29 10:17:39 -0700

    agasaf

--- src/jfo-section-8/ShoppingCart.java
+++ src/jfo-section-8/ShoppingCart.java
@@ -1,12 +1,19 @@
 public class ShoppingCart {
 
     public static void main(String[] args) {
-        Calculator calc = new Calculator();
+            Calculator calc = new Calculator();
         int addResult = calc.add(43, 79);
         System.out.println("Add Result: " + addResult);
-
+        try 
+        {
         double divResult = calc.divide(15, 0);
         System.out.println("Division Result: " + divResult);
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println("dont divide by 0 troll");
+            e.printStackTrace();
+        }
 
     }
 }
